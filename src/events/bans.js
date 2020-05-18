35a3bc3e8e1351dba2aaa6add541cb43cc57f36a
Wong Zhao <wz68.scratch@gmail.com>
2020-05-17 19:24:53 -0700

    Export unban event properly

--- src/events/bans.js
+++ src/events/bans.js
@@ -1,14 +1,7 @@
 import log from "../logger";
 
-async function guildBanRemove(guild, user) {
+export async function guildBanRemove(guild, user) {
     const removed = await this.agenda.cancel({ name: "unban", "data.guild": guild.id, "data.user": user.id });
 
     if (removed > 0) log.debug(`Cancelling Agenda job for user ${user.username} unbanned in ${guild.name}`);
 }
-
-export const events = [
-    {
-        name: "guildBanRemove",
-        run: guildBanRemove
-    }
-];
